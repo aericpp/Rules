29c2edb90966e111b7e9b204288a4bbf20f0d52f
aericpp <p.yue89@gmail.com>
2024-09-16 12:31:51 +0800

    test ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,27 +6,24 @@ sudo apt install -qy git
 
 CURRENT_PATH=$(pwd)
 
-git clone https://github.com/aericpp/Rules.git
 git clone https://github.com/v2fly/domain-list-community.git
 cd "${CURRENT_PATH}/domain-list-community"
 go mod download
-go run ./ --datapath="${CURRENT_PATH}/Rules/v"
+go run ./ --datapath="${CURRENT_PATH}/v"
 
 test -d "${CURRENT_PATH}/release" || mkdir "${CURRENT_PATH}/release"
 cp "${CURRENT_PATH}/domain-list-community/dlc.dat" "${CURRENT_PATH}/release/"
 
-cd "${CURRENT_PATH}/Rules/"
+cd "${CURRENT_PATH}"
 tar cvzf "${CURRENT_PATH}/release/surge.tar.gz" surge
 tar cvzf "${CURRENT_PATH}/release/autoproxy.tar.gz" autoproxy
 tar cvzf "${CURRENT_PATH}/release/shadowrocket.tar.gz" shadowrocket
 
-cp -R "${CURRENT_PATH}/Rules/surge" "${CURRENT_PATH}/release/"
-cp -R "${CURRENT_PATH}/Rules/autoproxy" "${CURRENT_PATH}/release/"
-cp -R "${CURRENT_PATH}/Rules/shadowrocket" "${CURRENT_PATH}/release/"
+cp -R "${CURRENT_PATH}/surge" "${CURRENT_PATH}/release/"
+cp -R "${CURRENT_PATH}/autoproxy" "${CURRENT_PATH}/release/"
+cp -R "${CURRENT_PATH}/shadowrocket" "${CURRENT_PATH}/release/"
 
 ls -alh "${CURRENT_PATH}/"
-
-ls -alh "${CURRENT_PATH}/Rules/"
 
 ls -alh "${CURRENT_PATH}/release/"
 
